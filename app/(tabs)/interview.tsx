68e0042f41a6184c5f2af40127d862b598e9aeb7
EMBKSM <k33827925@gmail.com>
2025-11-12 13:45:48 +0900

    re commit

--- app/(tabs)/interview.tsx
+++ app/(tabs)/interview.tsx
@@ -4,7 +4,6 @@ import tw from "twrnc";
 import { Text, View } from "@/components/Themed";
 
 export default function AiInterviewScreen() {
-  // 함수 이름 변경
   return (
     <View style={styles.container}>
       <Text style={tw`text-blue-500 text-3xl font-bold`}>AI 인터뷰 화면</Text>
